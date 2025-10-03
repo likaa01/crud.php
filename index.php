c684de59061eaf789719d89413a2fb6b1982eea1
likaa01 <malikalika100901@gmail.com>
2025-10-03 10:50:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>CRUD PHP MySQL Bootstrap</title>
+  </head>
+
+  <body>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="container mt-5">
+</nav>
+  <h1>Data Barang</h1>
+  <table class="table table-bordered table-striped">
+      <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Jumlah</th>
+            <th>Harga</th>
+            <th>Tanggal</th>
+            <th>Aksi</th>
+        </tr>  
+    </thead> 
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Mouse</td>
+            <td>4</td>
+            <td>85000</td>
+            <td>02/08/2025</td>
+            <td width="15%" class="text-center">
+                <button type="button" class="btn btn-success">Edit</button>
+                <button type="button" class="btn btn-danger">Hapus</button>
+            </td>
+        </tr>
+    </tbody>
+</table>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
